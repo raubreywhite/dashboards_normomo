5b56c743ad7c19e13a876fb9f50cba8eb13b65ed
Travis CI User <travis@example.org>
2018-11-06 16:45:01 +0000

    Travis CI update 10 [ci skip]

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: normomo")
-  packageStartupMessage("Version 2018.11.02")
+  packageStartupMessage("Version 2018.11.06")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
